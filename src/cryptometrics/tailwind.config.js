560f46574fce638b97a40477d471f185e192c1b0
Himanshu Aggarwal <aggarwah@mcmaster.ca>
2022-02-19 13:59:53 -0500

    Configure tailwind with additional CSS properties

--- src/cryptometrics/tailwind.config.js
+++ src/cryptometrics/tailwind.config.js
@@ -4,7 +4,28 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+        dm: "'DM Sans', sans-serif",
+        inter: "'Inter', sans-serif",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+        "5xl": "3rem",
+        "6xl": "4rem",
+      },
+      borderWidth: {
+        1: "1px",
+      },
+      colors: {
+        dark: {
+          700: "#242731",
+          800: "#1f2128",
+          900: "#191b20",
+        },
+      },
+    },
   },
   plugins: [],
 };
