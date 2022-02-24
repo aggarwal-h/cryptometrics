7cf1d8791310007a5051dd0e00146fa0108414f3
Himanshu Aggarwal <aggarwah@mcmaster.ca>
2022-02-24 13:51:48 -0500

    Add loading bar on page route change

--- src/cryptometrics/pages/_app.js
+++ src/cryptometrics/pages/_app.js
@@ -1,6 +1,13 @@
 import "../styles/globals.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 const queryClient = new QueryClient();
 
