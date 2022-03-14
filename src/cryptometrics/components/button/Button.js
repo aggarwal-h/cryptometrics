b5812f8af8a7168f562e97ff893f629e82d073e9
Himanshu Aggarwal <aggarwah@mcmaster.ca>
2022-03-13 23:54:57 -0400

    Add type prop to Button component

--- src/cryptometrics/components/button/Button.js
+++ src/cryptometrics/components/button/Button.js
@@ -1,11 +1,12 @@
 import classNames from "classnames";
 import React from "react";
 
-export default function Button({ children, className, onClick }) {
+export default function Button({ children, className, onClick, type }) {
   return (
     <button
       className={classNames("py-2 px-5 rounded-xl", className)}
       onClick={onClick}
+      type={type ? type : "button"}
     >
       {children}
     </button>
