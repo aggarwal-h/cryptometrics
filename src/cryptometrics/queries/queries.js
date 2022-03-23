fc33921d7090b6991a4c9132380f1162876cc847
Himanshu Aggarwal <aggarwah@mcmaster.ca>
2022-03-23 12:07:38 -0400

    Update query to retrieve candlestick data

--- src/cryptometrics/queries/queries.js
+++ src/cryptometrics/queries/queries.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import moment from "moment";
 import { useQuery } from "react-query";
 
 export function useCryptoTimeSeriesData(name, days = 7, interval = "daily") {
@@ -18,15 +19,29 @@ export function useCryptoTimeSeriesData(name, days = 7, interval = "daily") {
   );
 }
 
-export function useCryptoTimeSeriesRangeData(name, from, to) {
+export function useCryptoTimeSeriesRangeData(
+  name,
+  from,
+  to,
+  candlestick = false
+) {
+  let url = `https://api.coingecko.com/api/v3/coins/${name}/market_chart/range?vs_currency=usd&from=${from}&to=${to}`;
+  if (candlestick) {
+    const validDays = [1, 7, 14, 30, 90, 180, 365];
+    const requestedDays = moment.unix(to).diff(moment.unix(from), "days");
+    const finalDays = validDays.reduce((prev, curr) =>
+      Math.abs(curr - requestedDays) < Math.abs(prev - requestedDays)
+        ? curr
+        : prev
+    );
+    url = `https://api.coingecko.com/api/v3/coins/${name}/ohlc?vs_currency=usd&days=${finalDays}`;
+  }
   return useQuery(
-    `${name}_from_${from}_to_${to}`,
+    `${name}_from_${from}_to_${to}_${
+      candlestick ? "with" : "without"
+    }_candlestick`,
     () => {
-      return axios
-        .get(
-          `https://api.coingecko.com/api/v3/coins/${name}/market_chart/range?vs_currency=usd&from=${from}&to=${to}`
-        )
-        .then((res) => res.data);
+      return axios.get(url).then((res) => res.data);
     },
     {
       refetchOnWindowFocus: false,
