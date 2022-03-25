d5ffa59b634bc641312e064fbf10b956be7d1a9d
Himanshu Aggarwal <aggarwah@mcmaster.ca>
2022-03-25 13:29:55 -0400

    Make inputs responsive

--- src/cryptometrics/components/inputs/SearchInput.js
+++ src/cryptometrics/components/inputs/SearchInput.js
@@ -6,7 +6,7 @@ function SearchInput({ onChange, value, onFocus, onBlur, initialValue }) {
   return (
     <div className="relative">
       <Input
-        className="w-52 h-12 text-base font-semibold outline-none dark:text-white px-3 pr-10 rounded-2xl bg-dark-700"
+        className="w-52 h-12 text-base font-semibold outline-none dark:text-white px-3 pr-10 rounded-2xl bg-gray-100 dark:bg-dark-500"
         placeholder="Search"
         type="text"
         onChange={onChange}
